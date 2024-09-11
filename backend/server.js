0efe954a27a053b1ddf5788c905935a9aae37600
unknown <veerakumarpalanisamy405@gmail.com>
2024-09-11 09:50:08 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+ 
+import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongoDb.js";
+
+dotenv.config();
+
+
+const app = express();
+const PORT = process.env.PORT || 5000; 
+
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true })); // to parse form data (urlencoded)
+
+
+app.use("/api/auth",authRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+    connectMongoDB();
+}); 
