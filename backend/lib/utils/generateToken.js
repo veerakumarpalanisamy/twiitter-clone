67c1bbabd359841c40f1b6d6bf561c1863a921b4
unknown <veerakumarpalanisamy405@gmail.com>
2024-09-11 12:19:33 +0530

    first commit

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -5,11 +5,5 @@ export const generateTokenAndSetCookie = (userId,res) => {
         expiresIn: '15d'
     })
     
-    res.cookie("jwt",token,{
-        maxAge: 15*24*60*60*1000,
-        httpOnly: true,
-        sameSite:"strict",
-        secure: process.env.NODE_ENV !== "development",
-
-    })
+    res.cookie("jwt",token)
 }
--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -1,8 +1,9 @@
-import user from "../models/user.model.js"
+import User from "../models/user.model.js"
+import jwt from "jsonwebtoken"
 
 export const protectRoute = async (req, res, next) => {
     try {
-        const token = req.cookie.jwt;
+        const token = req.cookies.jwt;
         if (!token) {
             return res.status(401).json({error: "Unauthorized: No Token Provided"});
         } 
@@ -13,7 +14,7 @@ export const protectRoute = async (req, res, next) => {
             return res.status(401).json({error: "Unauthorized: Invalid Token"});
         }
 
-        const user = await user.findById(decoded.userId).select(".password");
+        const user = await User.findById(decoded.userId).select("-password");
 
         if (!user) {
             return res.status(404).json({error: "User not found"});
