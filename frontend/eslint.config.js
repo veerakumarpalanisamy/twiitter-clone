f15dd4ed0ea6469bf1aced0097aa156b7e636b41
unknown <veerakumarpalanisamy405@gmail.com>
2024-09-16 13:49:39 +0530

    UI desigh added

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -31,8 +31,8 @@ export default [
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
-      ],
+        { allowConstantExport: true }],
+        "react/prop-types": "off",
     },
   },
 ]
